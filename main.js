2d43d2b9fe7cc8dce8f64081bc0304d016e13f2f
Brian <lbrian357@gmail.com>
2025-09-03 19:49:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -33,8 +33,8 @@ var rpLib = {
       rpLib.utils.initCitySelection((brandId) => {
         // Load existing data into #alert-bar-text
         // Get brand slug -> send GET request to proxy api to get brand cms details -> populate the input field with data 
-        const selectedCitySlug = $("#city-select option:selected").attr("data-slug");
-        const brandPromise = rpLib.settingsPage.fetchBrandDetails(selectedCitySlug);
+        const selectedCityId = $("#city-select option:selected").val();
+        const brandPromise = rpLib.settingsPage.fetchBrandDetails(selectedCityId);
         Promise.all([brandPromise]).then(() => {
           $(".#alert-bar-text").text(brandResponse.fieldData["alert-bar-text"]);
         });
