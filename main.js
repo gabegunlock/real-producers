3d55307bdf36a7bf79caf39036c25ac5740f6cee
Brian <lbrian357@gmail.com>
2025-09-03 17:18:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -37,8 +37,8 @@ var rpLib = {
       // Load existing alert-bar-text
         // Get brand slug -> send GET request to proxy api to get brand cms details -> populate the input field with data 
       const selectedCitySlug = $("#city-select option:selected").attr("data-slug");
-      const brandPromises = await rpLib.fetchBrandDetails(selectedCitySlug);
-      Promise.all(fetchBrandPromises).then(() => {
+      const brandPromise = rpLib.fetchBrandDetails(selectedCitySlug);
+      Promise.all([brandPromise]).then(() => {
         $(".#alert-bar-text").text(brandResponse.fieldData["alert-bar-text"]);
       });
       
